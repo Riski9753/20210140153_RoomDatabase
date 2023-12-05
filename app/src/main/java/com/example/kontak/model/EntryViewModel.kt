c680b21582f86c29ad73d6db98a9803529c9a3d0
RiskI <riskianandapratama14@gmail.com>
2023-12-05 19:23:16 +0700

    Menambahkan data class

--- app/src/main/java/com/example/kontak/model/EntryViewModel.kt
+++ app/src/main/java/com/example/kontak/model/EntryViewModel.kt
@@ -1,2 +1,19 @@
 package com.example.kontak.model
 
+import android.app.PictureInPictureUiState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class EntryViewModel(private  val repostoriSiswa: RpostoriSiswa): ViewModel(){
+
+    data class UIStateSiswa(
+        val detailSiswa: DetailSiswa = DetailSiswa(),
+        val isEntryValid: Boolean = false
+    )
+    data class DetailSiswa(
+        val id: Int = 0,
+        val nama: String = "",
+        val alamat: String = "",
+        val telpon: String = "",
+    )
+}
