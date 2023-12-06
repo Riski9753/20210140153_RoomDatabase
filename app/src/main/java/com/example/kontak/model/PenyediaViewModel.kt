57c8c7e00767bd6ad39ad018aaeaf851a5ef6da9
RiskI <riskianandapratama14@gmail.com>
2023-12-06 14:42:29 +0700

    Menambahkan AplikasiSiswa

--- app/src/main/java/com/example/kontak/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/kontak/model/PenyediaViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.kontak.model
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.kontak.AplikasiSiswa
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
