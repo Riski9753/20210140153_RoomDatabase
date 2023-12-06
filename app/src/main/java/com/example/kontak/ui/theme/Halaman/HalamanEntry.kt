a625e5dc0b59ef92dd9c1991b42c4214693b5c6e
RiskI <riskianandapratama14@gmail.com>
2023-12-06 15:33:24 +0700

    Menambahkan composhibel

--- app/src/main/java/com/example/kontak/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/kontak/ui/theme/Halaman/HalamanEntry.kt
@@ -1,2 +1,102 @@
 package com.example.kontak.ui.theme.Halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.kontak.R
+import com.example.kontak.model.EntryViewModel
+import com.example.kontak.model.PenyediaViewModel
+import com.example.kontak.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.launch
+
+
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry
+}
+
+
+
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: EntryViewModel.UIStateSiswa,
+    onSiswaValueChange: (EntryViewModel.DetailSiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier =Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_lare)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {}
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailsiswa: EntryViewModel.DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (EntryViewModel.DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ){
+        OutlinedTextField(
+            value = detailsiswa.nama,
+            onValueChange = {onValueChange(detailsiswa.copy(nama = it))},
+            label = { Text(stringResource(R.string.nama)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailsiswa.alamat ,
+            onValueChange = {onValueChange(detailsiswa.copy(alamat = it))},
+            label = { Text(stringResource(R.string.alamat)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailsiswa.telpon,
+            onValueChange = {onValueChange(detailsiswa.copy(telpon = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(text = stringResource(R.string.tlp)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        if (enabled) {
+            Text(
+                text = stringResource(R.string.required ),
+                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+        Divider(
+            thickness = dimensionResource(R.dimen.padding_small),
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium))
+        )
+    }
+}
