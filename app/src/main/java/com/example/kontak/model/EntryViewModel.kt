eb35ea93c7e47b5b413a36bb428300173099c6df
RiskI <riskianandapratama14@gmail.com>
2023-12-06 14:14:14 +0700

    Menambahkan fun detail, fun siswa,

--- app/src/main/java/com/example/kontak/model/EntryViewModel.kt
+++ app/src/main/java/com/example/kontak/model/EntryViewModel.kt
@@ -1,11 +1,28 @@
 package com.example.kontak.model
 
 import android.app.PictureInPictureUiState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.kontak.Repositori.RepositoriSiswa
+import com.example.kontak.data.Siswa
 
-class EntryViewModel(private  val repostoriSiswa: RpostoriSiswa): ViewModel(){
+class EntryViewModel(private  val repostoriSiswa: RepositoriSiswa): ViewModel(){
 
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    suspend fun saveSiswa(){
+        if(validasiInput()){
+            repostoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
     data class UIStateSiswa(
         val detailSiswa: DetailSiswa = DetailSiswa(),
         val isEntryValid: Boolean = false
@@ -15,5 +32,21 @@ class EntryViewModel(private  val repostoriSiswa: RpostoriSiswa): ViewModel(){
         val nama: String = "",
         val alamat: String = "",
         val telpon: String = "",
+    )
+    fun  DetailSiswa.toSiswa(): Siswa = Siswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon
+    )
+    fun Siswa.toUiStateSiswa(isEntryValid: Boolean= false): UIStateSiswa = UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
+    fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon
     )
 }
